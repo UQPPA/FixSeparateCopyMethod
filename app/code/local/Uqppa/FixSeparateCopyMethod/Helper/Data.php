595cf0a53cc48904939b24f338f44e48910f0519
Adrián Martínez <adrian.martinez@interactiv4.com>
2016-02-05 13:16:40 +0100

    Fixed to work also with bcc and separate emails method

--- app/code/local/Uqppa/FixSeparateCopyMethod/Helper/Data.php
+++ app/code/local/Uqppa/FixSeparateCopyMethod/Helper/Data.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: adrianmartinez
- * Date: 05/02/16
- * Time: 02:31
- */ 
+
 class Uqppa_FixSeparateCopyMethod_Helper_Data extends Mage_Core_Helper_Abstract {
 
 }
